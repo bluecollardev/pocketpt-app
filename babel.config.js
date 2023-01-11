30cd5916bb7aa8a5a53a933904e2107633e816f5
Lucas Lopatka <lucas@bluecollardev.com>
2023-01-11 23:57:40 +0700

    Update babel config / metro config / tsconfig. Get path aliases working.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,35 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      // ["module:react-native-dotenv"],
+      [
+        "module-resolver",
+        {
+          alias: {
+            "^mediashare/(.*)": "./app/src/\\1",
+          },
+          /* resolvePath(sourcePath, currentFile) {
+            if (
+              sourcePath === "react-native" &&
+              !(
+                (
+                  currentFile.includes("node_modules/react-native/") || // macos/linux paths
+                  currentFile.includes("node_modules\\react-native\\")
+                ) // windows path
+              ) &&
+              !(
+                currentFile.includes("resolver/react-native/") ||
+                currentFile.includes("resolver\\react-native\\")
+              )
+            ) {
+              return path.resolve(__dirname, "resolver/react-native");
+            }
+            return undefined;
+          }, */
+        },
+      ],
+    ],
   };
 };
